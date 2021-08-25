8871dd878ba7e6f49cbba5ced993fda220c322e5
Alejandro Ferrín <aferrin.dev@gmail.com>
2021-08-25 16:45:18 +0200

    Añadida lectura directa de tarjeta en formulario nuevo usuario,eliminada página de lectura de tarjeta, otros cambios estéticos

--- src/main/java/taquillas/controller/CardController.java
+++ src/main/java/taquillas/controller/CardController.java
@@ -2,6 +2,7 @@ package taquillas.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +16,18 @@ public class CardController {
     @Autowired
     private CardReader cardReader;
 
+    /* Deprecated
     @GetMapping("/card")
     public String redirect(Model model) {
         String cardNumber = cardReader.getRead();
         model.addAttribute("num_tarjeta", cardNumber);
         return "num_tarjeta";
+    }
+    */
+    @GetMapping("/number")
+    public ResponseEntity<String> getNumber() {
+        String cardNumber = cardReader.getRead();
+        return ResponseEntity.ok(cardNumber);
     }
 
     @GetMapping()
