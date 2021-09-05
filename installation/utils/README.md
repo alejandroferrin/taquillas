3ad0ae733346a8b12378a98fff2fe129707e499f
Alejandro Ferrín <aferrin.dev@gmail.com>
2021-09-05 15:40:50 +0200

    Cambios script access_point

--- installation/utils/README.md
+++ installation/utils/README.md
@@ -2,3 +2,5 @@
 ## Crear un Access Point para acceder via wifi a una Raspberry Pi 4 aislada de la red.
 
 `curl -s https://raw.githubusercontent.com/alejandroferrin/taquillas/main/installation/utils/configure-access-point | sudo bash`
+
+Ip del Access Point: __192.168.4.1__
--- installation/utils/access_point_backup.sh
+++ installation/utils/access_point_backup.sh
@@ -2,26 +2,33 @@
 echo "#####################__Actualizando__"
 sudo apt-get update && sudo apt-get -y upgrade
 
-sudo apt -y install dnsmasq hostapd
-
+sudo apt -y install hostapd
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
+sudo apt -y install dnsmasq
+sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 sudo echo "" >> /etc/dhcpcd.conf
 sudo echo "interface wlan0" >> /etc/dhcpcd.conf
 sudo echo "static ip_address=192.168.4.1/24ᅾ" >> /etc/dhcpcd.conf
+sudo echo "nohook wpa_supplicant" >> /etc/dhcpcd.conf
 
-
-
+sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/routed-ap.conf
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo netfilter-persistent save
 
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo echo "interface=wlan0 # Listening interface" >> /etc/dnsmasq.conf
 sudo echo "dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h" >> /etc/dnsmasq.conf
+sudo echo "domain=wlan     # Local wireless DNS domain" >> /etc/dnsmasq.conf
+sudo echo "address=/taquillas/192.168.4.1" >> /etc/dnsmasq.conf
 
+sudo rfkill unblock wlan
 
+sudo echo "country_code=ES" >> /etc/hostapd/hostapd.conf
 sudo echo "interface=wlan0" >> /etc/hostapd/hostapd.conf
-sudo echo "driver=nl80211" >> /etc/hostapd/hostapd.conf
 sudo echo "ssid=WIFI_TAQUILLAS" >> /etc/hostapd/hostapd.conf
-sudo echo "hw_mode=g" >> /etc/hostapd/hostapd.conf
-sudo echo "channel=7" >> /etc/hostapd/hostapd.conf
-sudo echo "wmm_enabled=0" >> /etc/hostapd/hostapd.conf
+sudo echo "hw_mode=a" >> /etc/hostapd/hostapd.conf
+sudo echo "channel=36" >> /etc/hostapd/hostapd.conf
 sudo echo "macaddr_acl=0" >> /etc/hostapd/hostapd.conf
 sudo echo "auth_algs=1" >> /etc/hostapd/hostapd.conf
 sudo echo "ignore_broadcast_ssid=0" >> /etc/hostapd/hostapd.conf
@@ -31,19 +38,6 @@ sudo echo "wpa_key_mgmt=WPA-PSK" >> /etc/hostapd/hostapd.conf
 sudo echo "wpa_pairwise=TKIP" >> /etc/hostapd/hostapd.conf
 sudo echo "rsn_pairwise=CCMP" >> /etc/hostapd/hostapd.conf
 sudo echo "" >> /etc/hostapd/hostapd.conf
-
-sudo echo "" >> /etc/default/hostapd
-sudo echo "DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" >> /etc/default/hostapd
-
-sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
-
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
-
-sudo sed -i '$d' /etc/rc.local
-sudo echo "iptables-restore < /etc/iptables.ipv4.nat" >> /etc/rc.local
-sudo echo "exit 0" >> /etc/rc.local
-
 
 #sudo systemctl reboot
 
